42380243a7e41e96f5f419aae9aba09b432b8a60
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-05 12:14:37 +0600

    home layout setup

--- src/layout/HomeLayOut.jsx
+++ src/layout/HomeLayOut.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router'
 
 export default function HomeLayOut() {
   return (
@@ -6,8 +7,8 @@ export default function HomeLayOut() {
       <nav>
 
       </nav>
-      <main>
-
+      <main className='max-w-7xl mx-auto'>
+        <Outlet></Outlet>
       </main>
       <footer></footer>
     </div>
