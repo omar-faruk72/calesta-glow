ced91e9b45fe7b95fabc7bd9d7bcd5af1055d803
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-05 12:11:54 +0600

    home layout createt

---
+++ src/layout/HomeLayOut.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function HomeLayOut() {
+  return (
+    <div>
+      <nav>
+
+      </nav>
+      <main>
+
+      </main>
+      <footer></footer>
+    </div>
+  )
+}
